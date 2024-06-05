5efdcb414da39a9a9b7623010a33bcb7fd510788
krushil1 <krushilamrutiya@gmail.com>
2024-06-05 14:47:27 -0700

    Vercel Analytics

--- src/index.js
+++ src/index.js
@@ -15,6 +15,8 @@ import React from "react";
 import { createRoot } from "react-dom";
 import "./index.css";
 import App from "./App";
+import { Analytics } from "@vercel/analytics/react"
+
 
 // Disable lazy loading for all images in the application
 const imgElements = document.querySelectorAll('img');
@@ -25,5 +27,6 @@ const root = createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
+    <Analytics />
   </React.StrictMode>
 );
